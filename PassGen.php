673e30c4be1e6d0edf51bccad48d0cf45aa7598d
tyleryarnelli <tyleryarnelli35@gmail.com>
2014-08-07 10:56:28 -0400

    Update PassGen.php
    Cleaned up code.

--- PassGen.php
+++ PassGen.php
@@ -18,62 +18,7 @@ function PassGen(){
 <head>
 
 	<title>Password Generator</title>
-	
-	<style type="text/css">
-	
-		body {
-			color: white;
-			background-color: #003;
-			margin: 0px
-			}
-			
-		input {
-			text-align: center; 
-		}
-
-		#horizon {
-			color: white;
-			background-color: transparent;
-			text-align: center;
-			position: absolute;
-			top: 30%;
-			left: 0px;
-			width: 100%;
-			height: 1px;
-			overflow: visible;
-			visibility: visible;
-			display: block
-			}
-
-		#content {
-			font-family: Verdana, Geneva, Arial, sans-serif;
-			background-color: transparent;
-			margin-left: -125px;
-			position: absolute;
-			top: -35px;
-			left: 50%;
-			width: 250px;
-			height: 70px;
-			visibility: visible
-			}
-			
-		#pass {
-			//width:250px;
-			padding:10px;
-			border:5px dashed #0000B5;
-			margin:10px;
-		}
-
-		.bodytext {
-			font-size: 14px
-			}
-			
-		.headline {
-			font-weight: bold;
-			font-size: 34px
-			}
-	
-	</style>
+	<link rel="stylesheet" type="text/css" href="style.css" />
 	
 </head>
 
